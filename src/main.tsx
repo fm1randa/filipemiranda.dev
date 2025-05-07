7931bfb7d5f68bc3ef7608a77964513bbdf574e9
Filipe Miranda <email@filipemiranda.dev>
2025-05-07 11:40:21 -0300

    chore: update package dependencies and scripts to use vite-react-ssg for development, build, and preview

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import App from './App.tsx';
-import { ExternalRedirect } from './components/ExternalRedirect';
-import './i18n';
-import './index.css';
+import { RouteRecord, ViteReactSSG } from "vite-react-ssg";
+import App from "./App.tsx";
+import { ExternalRedirect } from "./components/ExternalRedirect";
+import "./i18n";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/meet"
-            element={
-              <ExternalRedirect to="https://tidycal.com/filipemiranda" />
-            }
-          />
-          <Route path="/" element={<App />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </StrictMode>
-  );
-}
+const routes: RouteRecord[] = [
+  {
+    path: "meet",
+    element: <ExternalRedirect to="https://tidycal.com/filipemiranda" />,
+  },
+  {
+    index: true,
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <ExternalRedirect to="/" />,
+  },
+];
+
+export const createRoot = ViteReactSSG({ routes });
